a0a5435e306138a2b90b9311d6144cc64da7ca0e
PatrykKruppa <128226968+PatrykKruppa@users.noreply.github.com>
2023-06-23 21:52:51 +0200

    Add files via upload

---
+++ 4.5_sortowanie przez wybieranie.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class MyProgram
+{
+    private static Random random = new Random();
+
+    public static void Main(string[] args)
+    {
+        int Rozmiar, i;
+
+        Rozmiar = 10;
+        int[] tab = new int[Rozmiar];
+
+        for (i = 0; i <= Rozmiar - 1; i++)
+        {
+            tab[i] = random.Next(100);
+        }
+        for (i = 0; i <= Rozmiar - 1; i++)
+        {
+            Console.WriteLine(tab[i]);
+        }
+        int indeksNajmniejszegoElementu;
+
+        indeksNajmniejszegoElementu = 0;
+        int pozycjaDoZapisu;
+
+        for (pozycjaDoZapisu = 0; pozycjaDoZapisu <= Rozmiar - 1; pozycjaDoZapisu++)
+        {
+            indeksNajmniejszegoElementu = pozycjaDoZapisu;
+            for (i = pozycjaDoZapisu + 1; i <= Rozmiar - 1; i++)
+            {
+                if (tab[i] < tab[indeksNajmniejszegoElementu])
+                {
+                    indeksNajmniejszegoElementu = i;
+                }
+            }
+            int najmniejszaWartosc;
+
+            najmniejszaWartosc = tab[indeksNajmniejszegoElementu];
+            tab[indeksNajmniejszegoElementu] = tab[pozycjaDoZapisu];
+            tab[pozycjaDoZapisu] = najmniejszaWartosc;
+        }
+        Console.WriteLine("Po sortowaniu: ");
+        for (i = 0; i <= Rozmiar - 1; i++)
+       
+        {
+       
+            Console.WriteLine(tab[i]);
+
+        }
+        
+        Console.ReadLine();
+    }
+}
+
