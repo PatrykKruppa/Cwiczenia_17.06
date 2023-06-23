cab4b6c0eb837d9e5f96a12039f5cec9077bda2f
PatrykKruppa <128226968+PatrykKruppa@users.noreply.github.com>
2023-06-23 22:04:37 +0200

    Add files via upload

---
+++ 5.2.2_losowanie liczby parzystej.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int iloscLiczbParzystych = 10; // Ilość liczb parzystych do wylosowania
+        int licznikLosowan = 0; // Licznik losowań
+
+        Console.Write("Wersja 2: ");
+        for (int i = 0; i < iloscLiczbParzystych; i++)
+        {
+            int wylosowanaLiczba = random.Next(-4, 5) * 2;
+            licznikLosowan++;
+
+            Console.Write(wylosowanaLiczba);
+
+            if (i < iloscLiczbParzystych - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Liczba wykonanych losowań: " + licznikLosowan);
+        Console.ReadLine();
+    }
+}
+
---
+++ 5.3_+++.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+
+{
+
+    static void Main(string[] args)
+    {
+
+        //___+_
+        //__+_+_
+        //_+_+_+_
+        //+_+_+_+_
+
+
+        //Deklaracja zmiennych
+        int WysokoscTrojkata, plus, PusteMiejsca, i;
+
+
+        //wczytanie wysokosci trojkąta czyli ilosci wierszy
+        Console.Write("Jest to program, który narysuje trójkąt ze znaków +");
+        Console.Write("\nPodaj wysokość trójkąta: ");
+        WysokoscTrojkata = int.Parse(Console.ReadLine());
+
+        for (plus = 1; plus <= WysokoscTrojkata; plus++)
+        {
+
+            for (PusteMiejsca = 1; PusteMiejsca <= WysokoscTrojkata - plus; PusteMiejsca++)
+
+            {
+                Console.Write(" ");
+
+            }
+
+            for (i = 1; i <= plus; i++)
+
+            {
+
+                Console.Write("+ ");
+
+            }
+            Console.WriteLine();
+         
+
+        }
+
+
+    }
+
+}
